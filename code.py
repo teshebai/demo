8ab67771c4ba7b3d5c34241677e2a2ebbde338b3
Teshebayev <eraliteshebai2002gmail.com>
2023-01-28 17:06:44 +0600

    kvadrat function kurdik

--- code.py
+++ code.py
@@ -1,6 +1,11 @@
 # salem alem sozi
 print("Salem,alem")
 
-
+a=12
+res=kvadrat(a)
+print("a=",a)
 def title():
     print("Title")
+
+def kvadrat(a):
+    return a*a
--- demo.java
+++ demo.java
@@ -1,5 +1,12 @@
 public class Demo{
     public static void main(String[] args){
         System.out.println("Salem,alem!");
+        int a = 12;
+        int res;
+        res = kvadrat(a);
+        System.out.print(res);
+    }
+    public static int kvadrat(int a){
+        return a*a;
     }
 }
