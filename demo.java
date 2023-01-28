839d3cebef33994205edfaa3a55c382eeb53bcc8
Teshebayev <eraliteshebai2002gmail.com>
2023-01-28 16:58:51 +0600

    title metod add

--- demo.java
+++ demo.java
@@ -2,4 +2,7 @@ public class Demo{
     public static void main(String[] args){
         System.out.println("Salem,alem!");
     }
+    public static void title(){
+        System.out.println("TITLE");
+    }
 }
